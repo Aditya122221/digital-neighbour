035f5a127d33fed42e4b719db2fc1301b2044a50
Aditya122221 <adityakumar122221@gmail.com>
2025-12-02 17:40:30 +0530

    Refactor client configuration for improved readability: Removed unnecessary line breaks in the client.ts file, streamlining the code structure for better clarity and maintainability.

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from "next-sanity";
+import { createClient } from "next-sanity"
 
 export const client = createClient({
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
@@ -6,5 +6,4 @@ export const client = createClient({
 	apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-01",
 	useCdn: process.env.NODE_ENV === "production",
 	token: process.env.SANITY_API_READ_TOKEN,
-});
-
+})
